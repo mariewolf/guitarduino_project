fee05cf9b9d325baffdc27be718e2f15f809a7c6
Marie <marie.wolfel@email.ucr.edu>
2023-11-18 12:32:59 -0800

    basic task scheduler & timer inclusion
    setup with demonstration task initialization

---
+++ guitarduino/main/TickFuncs.h
@@ -0,0 +1,3 @@
+void Test() {
+    Serial.print("Test");
+}
---
+++ guitarduino/main/main.ino
@@ -0,0 +1,38 @@
+#include "Timer.h"
+#include "TickFuncs.h"
+
+int globalPeriod = 10;
+
+typedef struct task {
+  int (*func)(void);  //function pointer
+  int period;         //task period
+  int elapsedTime;    //time since last tick
+} task;
+
+const int num_tasks = 1;
+task tasks[num_tasks];
+
+void setup() {
+  Serial.begin(9600); //start serial comms
+  TimerSet(globalPeriod); //set timer period
+  TimerOn();
+
+  //initialize first task
+  tasks[0].func = &Test;
+  tasks[0].period = 1000;
+  tasks[0].elapsedTime = tasks[0].period;
+}
+
+void loop() {
+
+  for (int i = 0; i < num_tasks; i++) { //round-robin task execution
+    if (tasks[i].elapsedTime >= tasks[i].period) {
+      tasks[i].func();
+      tasks[i].elapsedTime = 0;
+    }
+    tasks[i].elapsedTime += globalPeriod;
+  }
+
+  while (!TimerFlag) {} //wait for timer period
+  TimerFlag = 0;
+}
